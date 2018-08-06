df51410ec9aa8381e480feea7901f81a92a90ee5
Matej Vukosav <vuki146@gmail.com>
2018-08-07 01:40:23 +0200

    Update support v4 items

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
@@ -1,20 +1,16 @@
 package com.example.xyzreader.ui;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.LoaderManager;
-import android.content.Loader;
 import android.database.Cursor;
 import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
-import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowInsets;
 
 import com.example.xyzreader.R;
 import com.example.xyzreader.data.ArticleLoader;
@@ -35,33 +31,39 @@ public class ArticleDetailActivity extends AppCompatActivity
 
     private ViewPager mPager;
     private MyPagerAdapter mPagerAdapter;
-    private View mUpButtonContainer;
-    private View mUpButton;
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
-        if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE );
-        }
+//        getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE );
         setContentView( R.layout.activity_article_detail );
 
-        getLoaderManager().initLoader( 0, null, this );
+        toolbar = findViewById( R.id.toolbar );
+        setSupportActionBar( toolbar );
+        ActionBar actionBar = getSupportActionBar();
+        if ( actionBar != null ) {
+            actionBar.setDisplayHomeAsUpEnabled( true );
+            actionBar.setDisplayShowHomeEnabled( true );
+            actionBar.setTitle( " " );
+        }
 
-        mPagerAdapter = new MyPagerAdapter( getFragmentManager() );
-        mPager = (ViewPager) findViewById( R.id.pager );
+        getSupportLoaderManager().initLoader( 0, null, this );
+
+        mPagerAdapter = new MyPagerAdapter( getSupportFragmentManager() );
+        mPager = findViewById( R.id.pager );
         mPager.setAdapter( mPagerAdapter );
         mPager.setPageMargin( (int) TypedValue
                 .applyDimension( TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics() ) );
         mPager.setPageMarginDrawable( new ColorDrawable( 0x22000000 ) );
 
-        mPager.setOnPageChangeListener( new ViewPager.SimpleOnPageChangeListener() {
+        mPager.addOnPageChangeListener( new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageScrollStateChanged( int state ) {
                 super.onPageScrollStateChanged( state );
-                mUpButton.animate()
+                toolbar.animate()
                         .alpha( ( state == ViewPager.SCROLL_STATE_IDLE ) ? 1f : 0f )
                         .setDuration( 300 );
             }
@@ -76,29 +78,6 @@ public class ArticleDetailActivity extends AppCompatActivity
             }
         } );
 
-        mUpButtonContainer = findViewById( R.id.up_container );
-
-        mUpButton = findViewById( R.id.action_up );
-        mUpButton.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick( View view ) {
-                onSupportNavigateUp();
-            }
-        } );
-
-        if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ) {
-            mUpButtonContainer.setOnApplyWindowInsetsListener( new View.OnApplyWindowInsetsListener() {
-                @Override
-                public WindowInsets onApplyWindowInsets( View view, WindowInsets windowInsets ) {
-                    view.onApplyWindowInsets( windowInsets );
-                    mTopInset = windowInsets.getSystemWindowInsetTop();
-                    mUpButtonContainer.setTranslationY( mTopInset );
-                    updateUpButtonPosition();
-                    return windowInsets;
-                }
-            } );
-        }
-
         if ( savedInstanceState == null ) {
             if ( getIntent() != null && getIntent().getData() != null ) {
                 mStartId = ItemsContract.Items.getItemId( getIntent().getData() );
@@ -108,7 +87,7 @@ public class ArticleDetailActivity extends AppCompatActivity
     }
 
     @Override
-    public Loader<Cursor> onCreateLoader( int i, Bundle bundle ) {
+    public android.support.v4.content.Loader<Cursor> onCreateLoader( int i, Bundle bundle ) {
         return ArticleLoader.newAllArticlesInstance( this );
     }
 
@@ -147,12 +126,12 @@ public class ArticleDetailActivity extends AppCompatActivity
     }
 
     private void updateUpButtonPosition() {
-        int upButtonNormalBottom = mTopInset + mUpButton.getHeight();
-        mUpButton.setTranslationY( Math.min( mSelectedItemUpButtonFloor - upButtonNormalBottom, 0 ) );
+        int upButtonNormalBottom = mTopInset + toolbar.getHeight();
+        toolbar.setTranslationY( Math.min( mSelectedItemUpButtonFloor - upButtonNormalBottom, 0 ) );
     }
 
-    private class MyPagerAdapter extends FragmentStatePagerAdapter {
-        public MyPagerAdapter( FragmentManager fm ) {
+    private class MyPagerAdapter extends android.support.v4.app.FragmentStatePagerAdapter {
+        public MyPagerAdapter( android.support.v4.app.FragmentManager fm ) {
             super( fm );
         }
 
@@ -167,7 +146,7 @@ public class ArticleDetailActivity extends AppCompatActivity
         }
 
         @Override
-        public Fragment getItem( int position ) {
+        public android.support.v4.app.Fragment getItem( int position ) {
             mCursor.moveToPosition( position );
             return ArticleDetailFragment.newInstance( mCursor.getLong( ArticleLoader.Query._ID ) );
         }
--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -1,13 +1,13 @@
 package com.example.xyzreader.ui;
 
-import android.app.LoaderManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -55,14 +55,14 @@ public class ArticleListActivity extends AppCompatActivity implements
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_article_list );
 
-        mToolbar = (Toolbar) findViewById( R.id.toolbar );
+        mToolbar = findViewById( R.id.toolbar );
 
         final View toolbarContainerView = findViewById( R.id.toolbar_container );
 
-        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById( R.id.swipe_refresh_layout );
+        mSwipeRefreshLayout = findViewById( R.id.swipe_refresh_layout );
 
-        mRecyclerView = (RecyclerView) findViewById( R.id.recycler_view );
-        getLoaderManager().initLoader( 0, null, this );
+        mRecyclerView = findViewById( R.id.recycler_view );
+        getSupportLoaderManager().initLoader( 0, null, this );
 
         if ( savedInstanceState == null ) {
             refresh();
